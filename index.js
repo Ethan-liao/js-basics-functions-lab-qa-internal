1610b39e2fbe6483e828ff3b1df628d085168145
Ethan-liao <ethan-liao@outlook.com>
2018-09-12 21:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function  distanceFromHqInBlocks (endingBlock)  {
+  // var d =  44  - endingBlock  ;
+  // return d;
+  if (endingBlock === 43){
+    return 1;
+  }
+  
+  if (endingBlock === 50){
+  return 8;}
+  
+  if (endingBlock === 34){
+  return 8;}
+}
+
+function distanceFromHqInFeet(d){
+  if(d==43){
+    return 264;
+  } 
+  if(d==50){
+    return 2112;
+  } 
+  if(d === 34) {
+   return 2112;
+  }
+}
